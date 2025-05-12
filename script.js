6c6b8e230388c09d899d51d9a4f37ba1b109007d
umeraltaf095 <umergujjar095@gmail.com>
2025-05-12 17:43:33 +0500

    Added Current digital time and day

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
 const handOfSecond = document.getElementById("handOfSecond");
 const handOfMinute = document.getElementById("handOfMinute");
 const handOfHour = document.getElementById("handOfHour")
+const displayTime =  document.getElementById("displayTime")
+const displayDate = document.getElementById("displayDate");
 const rotateSecond = 360/60 ; 
 const rotateMin = 360/60;
 const rotateHours = 360/12;
 console.log(rotateHours);
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 
 
 
 function time(){
 const currentDate = new Date();
+const day = days[currentDate.getDay()];
+const date = currentDate.getDate();
+const month = currentDate.getMonth();
+const year = currentDate.getFullYear();
+
+displayDate.textContent = `${date}/${month}/${year}`;
+
+
+
+
+
 const hours = currentDate.getHours();
 const minutes = currentDate.getMinutes();
 const seconds = currentDate.getSeconds();
- console.log(`${hours}: ${minutes}: ${seconds}`);
+displayTime.textContent = `${day}:${hours}:${minutes}:${seconds}`;
+//  console.log(`${hours}: ${minutes}: ${seconds}`);
 handOfSecond.style.transform = `translate(-50%, -100%) rotate(${seconds*rotateSecond}deg)`;
   handOfHour.style.transform = `translate(-50%, -100%) rotate(${hours*rotateHours}deg)`;
  handOfMinute.style.transform = `translate(-50%, -100%) rotate(${minutes*rotateMin}deg)`;
-console.log(minutes*rotateMin);
+
 
 
 
@@ -26,6 +42,9 @@ console.log(minutes*rotateMin);
 }
 
 setInterval(time,1000);
+
+
+
 
 
 
